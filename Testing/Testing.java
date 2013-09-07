a1ca3243731dd2aa20f108c5b278a09dae975c55
Fredghostkyle <fredghostkyle@yahoo.com>
2013-09-07 15:05:30 -0400

    cleanup for Testing.java

--- Testing/Testing.java
+++ Testing/Testing.java
@@ -1,10 +1,9 @@
-//A Very Simple Example
+//A Very Simple test
 class Testing {
 
    public static void main(String[] args){
 
         System.out.println("© fredghostkyle 2013. ALL RIGHTS RESERVED\nDo not copy or remake. Made in Java.");
         System.out.println("http://Fredghostkyle.com/CP");
-        //System.in.
    }
 }
