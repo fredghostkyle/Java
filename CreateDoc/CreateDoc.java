61a724cf4e06e152f320a34375bbec821ecb19a8
Fredghostkyle <fredghostkyle@yahoo.com>
2013-09-07 15:05:14 -0400

    fixes to CreateDoc.java

--- CreateDoc/CreateDoc.java
+++ CreateDoc/CreateDoc.java
@@ -1,27 +1,21 @@
+//Copyright @Fredghostkyle
+//2013 All Rights Reserved. 
 import java.io.*;
- 
 class CreateDoc {
-
-     public static void main(String args[])
+    public static void main(String args[])
   {
-//(c) fredghostkyle.
-//
-//Copyright info:
         System.out.println("© fredghostkyle 2013. ALL RIGHTS RESERVED\nDo not copy or remake. Made in Java.");
-        System.out.println("http://Fredghostkyle.com/CP");
-//*PROGRAM* 
-        System.out.println("\n \n \n");
-        System.out.println("Welcome. We are creating doing mkdir from .java Doc. Good luck");
-//creation
-final File parentDir = new File("crawl_html.TXT");
-parentDir.mkdir();
-final String hash = "txt";
-final String fileName = hash + ".txt";
-final File file = new File(parentDir, fileName);
-//i file.createNewFile(); 
-//created file "crawl_html/abc.txt"
-        System.out.println("Done!");
+        System.out.println("http://Fredghostkyle.com/CP"); //Copyright
+        System.out.println("\n \n");
+        System.out.println("Welcome. We are creating a folder called CreateDoc in / (where the java was decompiled)."); //Said what we are going to do
+        final File parentDir = new File("CreateDoc"); //Creation....
+        parentDir.mkdir();
+        final String hash = "txt";
+        final String fileName = hash + ".txt";
+        final File file = new File(parentDir, fileName);
+        //i file.createNewFile(); 
+        System.out.println("Done!"); //done!
   }
 }
-
-// if "//i"+shit is declared. It was there but is now removed from testing. 
+//NOTE:
+// if you see a "/i" comment infrunt of a string it was me messing with it to fix something. 
