646f6dbadcd3638dc97ccfb28a7eeaf0e227652d
fredghostkyle <fredghostkyle@yahoo.com>
2013-09-09 18:14:49 -0400

    added some comments
    (sudo code)

--- MPG/MPG.java
+++ MPG/MPG.java
@@ -4,16 +4,17 @@ class MPG {
      public static void main(String args[]) throws IOException
   {
 //start code
-   //floats
-   float drove;
-   float gallon;
-   float mpg;   //end floats 
+   //add floats
+   float drove; //line 14-15
+   float gallon; //line 16-17
+   float mpg; //none 
+   //end floats 
    Scanner keyboard = new Scanner (System.in);   
    System.out.println("Welcome!");
    System.out.println("Enter the amount of miles driven:"); //println for miles 
    drove = keyboard.nextFloat();  //set it to drove
    System.out.println("Enter how many gallons you have used on that drive:"); //println for gallons 
-  	gallon = keyboard.nextfloat();//set it to gallons
+   gallon = keyboard.nextfloat();//set it to gallons
    System.out.println("Miles driven: "+drove); //tells miles
    System.out.println("Gallons used: "+gallon); //tells gallons
 //end code
