760328813caacc4ac68ee1e1e242d536f143a696
Fredghostkyle <fredghostkyle@yahoo.com>
2013-09-09 19:37:59 -0400

    added MPG folder, CLASS, java.
    estemates MPG based off Gallons used and miles driven. has some sudo
    code in it.

--- MPG/MPG.java
+++ MPG/MPG.java
@@ -1,3 +1,4 @@
+//I suggest you get a Smart Car. 
 import java.io.*;
 import java.util.*; 
 class MPG {
@@ -7,16 +8,22 @@ class MPG {
    //add floats
    float drove; //line 14-15
    float gallon; //line 16-17
-   float mpg; //none 
+   //i float mpg; //not used  
    //end floats 
    Scanner keyboard = new Scanner (System.in);   
    System.out.println("Welcome!");
-   System.out.println("Enter the amount of miles driven:"); //println for miles 
+   System.out.println("Enter the exact amount of miles driven:"); //println for miles 
    drove = keyboard.nextFloat();  //set it to drove
-   System.out.println("Enter how many gallons you have used on that drive:"); //println for gallons 
-   gallon = keyboard.nextfloat();//set it to gallons
-   System.out.println("Miles driven: "+drove); //tells miles
+   System.out.println("Enter the exact amount of gallons you have used on that drive:"); //println for gallons 
+   gallon = keyboard.nextFloat();//set it to gallons
+   System.out.println("\nMiles driven: "+drove); //tells miles
    System.out.println("Gallons used: "+gallon); //tells gallons
+   System.out.println("Your miles per gallon (MPG) for that drive was: "+ drove / gallon); //drove divided by gallon = MPG
+   System.out.println("\nHappy driving!! "); //Goodbye!
+   //i c System.out.println("http://GitHub.com/fredghostkyle/Java/tree/master/MPG to see the code"); //link to code.  
 //end code
    }//end void
 }//end class
+
+// if you see "//i " that means its not needed. 
+// if you see "//i c "that means its not for class :P L
