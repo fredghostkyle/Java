5ab448a38de442a2f2766ceb2c1c3bdd9ff15ea7
Fredghostkyle <fredghostkyle@yahoo.com>
2013-09-10 16:27:37 -0400

    adds full code for Average_Calc
    Average_Calc's class is here. DESC. in block on top

--- Average_Calc/Average_Calc.java
+++ Average_Calc/Average_Calc.java
@@ -9,5 +9,25 @@ public class Average_Calc
 {
 	public static void main (String args[])
 	{
+	//add floats, num1, .. num5, ADV, sum
+	float num1 = 0, num2 = 0,	num3 = 0, num4 = 0, num5 = 0, ADV = 0, sum = 0;
+	//add Scaner
+	Scanner keyboard = new Scanner (System.in);
+    //start println
+	System.out.println("Welcome!"); //welcome
+	System.out.println("");
+	System.out.println("Number 1: ");
+	num1 = keyboard.nextFloat(); //get num1
+    System.out.println("Number 2: ");
+    num2 = keyboard.nextFloat(); //get num2
+    System.out.println("Number 3: ");
+    num3 = keyboard.nextFloat(); //get num 3
+    System.out.println("Number 4: ");
+    num4 = keyboard.nextFloat(); //get num 4
+    System.out.println("Number 5: "); 
+    num5 = keyboard.nextFloat(); //get num 5
+    sum = num1 + num2 + num3 + num4 + num5;
+    ADV = sum / 5;
+    System.out.println("Sum was: "+sum +". Average was "+ADV +".");
 	} //end main
 } //end class
