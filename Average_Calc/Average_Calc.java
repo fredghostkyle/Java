60e8961440f9155d2c3a9fef778c2951b0c45ce1
Fredghostkyle <fredghostkyle@yahoo.com>
2013-09-10 16:26:37 -0400

    basic ADV. calc.
    ...

---
+++ Average_Calc/Average_Calc.java
@@ -0,0 +1,13 @@
+/*  Author: Kyle
+	Disc: Calculates the avg. for 5 numbers. 
+    Creation: Sep. 10, 2013
+    Laste edit: 4:25 ET
+*/
+import java.io.*;
+import java.util.*;
+public class Average_Calc
+{
+	public static void main (String args[])
+	{
+	} //end main
+} //end class
