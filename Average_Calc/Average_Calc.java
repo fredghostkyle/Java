c75b2b0ea330cd71255991caa08250fbcdbf7f05
Fredghostkyle <fredghostkyle@yahoo.com>
2013-09-10 16:33:36 -0400

    adds .class

--- Average_Calc/Average_Calc.java
+++ Average_Calc/Average_Calc.java
@@ -1,5 +1,5 @@
 /*  Author: Kyle
-	Disc: Calculates the avg. for 5 numbers. 
+	Disc: Average_Calc :: finds the sum on num1, ... num5 and then displayes it to the user via. PrintLN.  
     Creation: Sep. 10, 2013
     Laste edit: 4:25 ET
 */
@@ -9,13 +9,12 @@ public class Average_Calc
 {
 	public static void main (String args[])
 	{
-	//add floats, num1, .. num5, ADV, sum
+	//add floats, num1, ... num5, ADV, sum
 	float num1 = 0, num2 = 0,	num3 = 0, num4 = 0, num5 = 0, ADV = 0, sum = 0;
 	//add Scaner
 	Scanner keyboard = new Scanner (System.in);
-    //start println
-	System.out.println("Welcome!"); //welcome
-	System.out.println("");
+//
+	System.out.println("Welcome!\n"); 
 	System.out.println("Number 1: ");
 	num1 = keyboard.nextFloat(); //get num1
     System.out.println("Number 2: ");
@@ -26,8 +25,8 @@ public class Average_Calc
     num4 = keyboard.nextFloat(); //get num 4
     System.out.println("Number 5: "); 
     num5 = keyboard.nextFloat(); //get num 5
-    sum = num1 + num2 + num3 + num4 + num5;
-    ADV = sum / 5;
+    sum = num1 + num2 + num3 + num4 + num5; //set sum. add num1, to num5 together.
+    ADV = sum / 5; //get ADV. Divide sum by 5 for the Adv.
     System.out.println("Sum was: "+sum +". Average was "+ADV +".");
 	} //end main
 } //end class
