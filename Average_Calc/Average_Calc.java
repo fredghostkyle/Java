1a375ece8214fc32ffc6827ddb0221acb07b8c02
fredghostkyle <fredghostkyle@yahoo.com>
2013-09-12 19:39:06 -0400

    fixed more spacing issues.

--- Average_Calc/Average_Calc.java
+++ Average_Calc/Average_Calc.java
@@ -7,15 +7,15 @@ import java.io.*;
 import java.util.*;
 public class Average_Calc
 {
-	public static void main (String args[])
-	{
-	//add floats, num1, ... num5, ADV, sum
-	float num1 = 0, num2 = 0,	num3 = 0, num4 = 0, num5 = 0, ADV = 0, sum = 0;
-	//add Scaner
-	Scanner keyboard = new Scanner (System.in);
+    public static void main (String args[])
+    {
+    //add floats, num1, ... num5, ADV, sum
+    float num1 = 0, num2 = 0,	num3 = 0, num4 = 0, num5 = 0, ADV = 0, sum = 0;
+    //add Scaner
+    Scanner keyboard = new Scanner (System.in);
 //
-	System.out.println("Welcome!\n"); 
-	System.out.println("Number 1: ");
+    System.out.println("Welcome!\n"); 
+    System.out.println("Number 1: ");
     num1 = keyboard.nextFloat(); //get num1
     System.out.println("Number 2: ");
     num2 = keyboard.nextFloat(); //get num2
